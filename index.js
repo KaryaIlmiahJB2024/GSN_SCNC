4ac8215e004d0a6e8ca7cbd613c6d7859bc7c9d5
NetzwerkGSN <69039053+NetzwerkGSN@users.noreply.github.com>
2024-07-14 00:23:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-const background = document.querySelector('.background');
 
-document.addEventListener('mousemove', (e) => {
-    const { clientX, clientY } = e;
-    const x = (clientX / window.innerWidth) * 100;
-    const y = (clientY / window.innerHeight) * 100;
-    background.style.backgroundPosition = `${x}% ${y}%`;
-});
